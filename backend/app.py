6fcaef547f4d7039ace039a2c794f670c77f2b2a
mehreen <zmehreen39@gmail.com>
2026-01-19 00:46:14 +0530

    added image upload functionality

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 @app.route('/')
 def home():
-    return "Hello AgriMentor! Backend is working."
+    return render_template('upload.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    image = request.files['image']
+
+    if image:
+        image_path = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
+        image.save(image_path)
+        return "Image uploaded successfully!"
+
+    return "No image uploaded"
 
 if __name__ == '__main__':
     app.run(debug=True)
