bd707ae70964b384d34eb2d523616f90a372a942
mehreen <zmehreen39@gmail.com>
2026-02-05 21:25:44 +0530

    Completed backend workflow with login and verification
    Implemented image upload, MySQL integration, request listing,
    verification workflow, and role-based login using Flask

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,28 @@
+from flask import session, redirect, url_for
+
 from flask import Flask, render_template, request
 import os
+import mysql.connector
 
 app = Flask(__name__)
+app.secret_key = "agrimentor_secret"
+
 
 UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# MySQL connection
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",        # agar password hai to daalo
+    database="agrimentor",
+    port=3307
+)
+
+
+cursor = db.cursor()
 
 @app.route('/')
 def home():
@@ -17,9 +35,55 @@ def upload():
     if image:
         image_path = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
         image.save(image_path)
-        return "Image uploaded successfully!"
+
+        query = "INSERT INTO uploads (image_name, status) VALUES (%s, %s)"
+        values = (image.filename, "pending")
+        cursor.execute(query, values)
+        db.commit()
+
+        return "Image uploaded & data saved to database!"
 
     return "No image uploaded"
+
+@app.route('/requests')
+def view_requests():
+    cursor.execute("SELECT * FROM uploads")
+    data = cursor.fetchall()
+    return render_template('requests.html', uploads=data)
+
+
+@app.route('/verify/<int:id>')
+def verify(id):
+    query = "UPDATE uploads SET status = 'verified' WHERE id = %s"
+    cursor.execute(query, (id,))
+    db.commit()
+    return "Request verified successfully!"
+
+@app.route('/login')
+def login_page():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+
+    query = "SELECT role FROM users WHERE username=%s AND password=%s"
+    cursor.execute(query, (username, password))
+    user = cursor.fetchone()
+
+    if user:
+        session['role'] = user[0]
+
+        if user[0] == 'user':
+            return redirect(url_for('home'))
+        elif user[0] == 'sub_admin':
+            return redirect(url_for('view_requests'))
+        else:
+            return "Admin logged in"
+    else:
+        return "Invalid login"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
